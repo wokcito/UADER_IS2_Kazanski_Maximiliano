c1b4a2241489717e5902e4c8f96ace5c62e811d8
Maximiliano Kazanski <maxikazanski@gmail.com>
2024-04-03 13:15:01 -0300

    feat: improve gpt.py

--- TP_02/gpt.py
+++ TP_02/gpt.py
@@ -2,58 +2,69 @@ import sys
 from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv()
+class Conversation():
+    def __init__(self, mode = None):
+        self.mode = mode
+        self.message_history = []
 
-CONVERSATION = "conversation"
-MODE = CONVERSATION if "--convers" in sys.argv else ""
-conversation = []
+        # generates OpenAI client
+        self.client = OpenAI()
 
-# generate OpenAI client
-client = OpenAI()
-
-def request(user_message, conversation = None):
-    if conversation is None:
-        conversation = []
-
-    messages = conversation + [
-        {
-            "role": "user",
-            "content": user_message
-        }
-    ]
-
-    response = client.chat.completions.create(
-        # frequency_penalty = 0,
-        # presence_penalty = 0
-        # temperature = 1,
-        # top_p = 1,
-        max_tokens = 4096,
-        messages = messages,
-        MODEl = "gpt-3.5-turbo-0125",
-    )
-
-    return response
-
-def main():
-    while True:
+    def get_user_message(self):
         try:
             user_message = input("Ingrese su consulta: ")
 
             if len(user_message) == 0:
-                raise Exception("Error: La consulta no debe estar vacía")
-
-            if MODE == CONVERSATION:
-                conversation.append({ "role": "user", "content": user_message })
+                raise Exception("Error: La consulta no debe estar vacía.")
 
             print("You: " + user_message)
-
-            response = request(user_message, conversation)
-
-            if MODE == CONVERSATION:
-                conversation.append(response.choices[0].message)
-
-            print("ChatGPT: " + response.choices[0].message.content)
+            return user_message
         except Exception as e:
             print(e)
 
-main()
+    def get_chatGPT_message(self, user_message):
+        try:
+            response = self.client.chat.completions.create(
+                # frequency_penalty = 0,
+                # presence_penalty = 0
+                # temperature = 1,
+                # top_p = 1,
+                max_tokens = 4096,
+                messages = self.message_history + [
+                    {
+                        "role": "user",
+                        "content": user_message
+                    }
+                ],
+                model = "gpt-3.5-turbo-0125",
+            )
+
+            chatGPT_message = response.choices[0].message.content
+
+            if self.mode == CONVERSATION:
+                self.message_history.append({ "role": "user", "content": user_message })
+                self.message_history.append({ "role": "assistant", "content": chatGPT_message })
+
+            print("ChatGPT: " + chatGPT_message)
+            return chatGPT_message
+        except Exception as e:
+            print(e)
+
+    def get_message_history(self):
+        return self.message_history
+
+load_dotenv()
+
+CONVERSATION = "conversation"
+MODE = CONVERSATION if "--convers" in sys.argv else ""
+conversation = Conversation(MODE)
+
+def main():
+    user_message = conversation.get_user_message()
+    conversation.get_chatGPT_message(user_message)
+
+if MODE == CONVERSATION:
+    while True:
+        main()
+else:
+    main()
